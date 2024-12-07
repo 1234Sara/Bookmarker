9003f4ddf4bf9a0b2fc83ca0992aa9f3ea5f1326
1234Sara <sarahabdo38@gmail.com>
2024-12-07 11:18:09 +0200

    third commit

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var siteNameInput = document.getElementById('siteName');
 var siteUrlInput = document.getElementById('siteUrl');
 var nameAlertInput = document.getElementById('nameAlert')
 var urlAlertInput = document.getElementById('urlAlert')
+var searchInput = document.getElementById('search')
 
 sitesUrl = []
 
@@ -15,10 +16,10 @@ if (website !== null){
 function subBtn(){
 
     for (let i = 0; i < sitesUrl.length; i++) {
-        if (sitesUrl[i].name.toLowerCase() === siteNameInput.value.toLowerCase()) {
+        if (sitesUrl[i].name.toLowerCase() === siteNameInput.value.toLowerCase() || sitesUrl[i].url.toLowerCase() === siteUrlInput.value.toLowerCase()) {
             Swal.fire({
-                title: "Duplicate Name",
-                text: "This site name already exists. Please choose a different name.",
+                title: "Duplicate Site Name or Site URL ",
+                text: "This site name or site url already exists. Please choose a different name.",
                 icon: "error"
             });
             return; 
@@ -74,6 +75,11 @@ function display(){
         cartona += `<tr class="line-table">
                         <td>${i + 1}</td>
                         <td>${sitesUrl[i].name}</td>
+                         <td><button class="btn btn-primary" onclick="setValues(${i})">
+                         <i class="fa fa-edit pe-2"></i>
+                         Update
+                         </button>
+                         </td>
                         <td>
                             <button class="btn visit">
                             <i class="fa-solid fa-eye pe-2"></i>
@@ -94,6 +100,8 @@ function display(){
 function clearInput(){
     siteNameInput.value=""
     siteUrlInput.value=""
+    siteNameInput.classList.remove('is-valid')
+    siteUrlInput.classList.remove('is-valid')
 }
 
 function deleteItem(index){
@@ -103,8 +111,85 @@ function deleteItem(index){
 }
 
 
+var superIndex = index;
+var index;
+
+function setValues(index){
+
+    superIndex = index;
+
+    document.getElementById('updateSite').style.display = "block"
+    document.getElementById('addSub').style.display = "none"
+
+    siteNameInput.value = sitesUrl[index].name
+    siteUrlInput.value = sitesUrl[index].url
+}
+
+
+function updateSite(){
+    
+    sitesUrl[superIndex].name = siteNameInput.value
+    sitesUrl[superIndex].url = siteUrlInput.value 
+    localStorage.setItem('site', JSON.stringify(sitesUrl))
+     display()
+     clearInput()
+     document.getElementById('updateSite').style.display = "none"
+     document.getElementById('addSub').style.display = "block"
+     for (let i = 0; i < sitesUrl.length; i++) {
+        if (sitesUrl[i].name.toLowerCase() === siteNameInput.value.toLowerCase() || sitesUrl[i].url.toLowerCase() === siteUrlInput.value.toLowerCase()) {
+            Swal.fire({
+                title: "Duplicate Site Name or Site URL ",
+                text: "This site name or site url already exists. Please choose a different name.",
+                icon: "error"
+            });
+            return; 
+        }
+    }
+     
+ }
+
+
+function searchWord(){
+
+    var newCartona = ""
+
+    for (var i = 0; i < sitesUrl.length; i++){
+        if (sitesUrl[i].name.toLowerCase().includes(searchInput.value.toLowerCase())){
+            newCartona += `
+            <tr class="line-table">
+                        <td>${i + 1}</td>
+                        <td>${sitesUrl[i].name}</td>
+                         <td><button class="btn btn-primary" onclick="setValues(${i})">
+                         <i class="fa fa-edit pe-2"></i>
+                         Update
+                         </button>
+                         </td>
+                        <td>
+                            <button class="btn visit">
+                            <i class="fa-solid fa-eye pe-2"></i>
+                            <a href="${sitesUrl[i].url}" target="_blank" class="text-decoration-none">Visit</a>
+                            </button>
+                        </td>
+                        <td>
+                            <button class="btn delete" onclick="deleteItem(${i})">
+                            <i class="fa-solid fa-trash-can"></i>
+                                Delete
+                            </button>
+                        </td>
+                    </tr>
+    `
+        }
+
+    }
+
+    document.getElementById('demo').innerHTML= newCartona
+    
+}    
+    
+
+
 function siteValidation(){
-    var regex = /^[a-zA-Z0-9_]{3,}$/
+    var regex = /^[a-zA-Z0-9_ ]{3,}$/
     if(regex.test(siteNameInput.value) == true){
         siteNameInput.classList.add('is-valid')
         siteNameInput.classList.remove('is-invalid')
